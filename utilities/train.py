b3f191d81395644b6a5c386351af3b08cc096253
chillz-the-first <vhenganitshilidzi846@gmail.com>
2026-07-01 19:19:13 +0200

    feat: creating cnn training loop function

--- utilities/train.py
+++ utilities/train.py
@@ -7,3 +7,17 @@ def train(model, X, y, loss_fn, optimizer, epochs=1000):
         optimizer.step()
 
     return model
+
+def train_cnn(model, loader, loss_fn, optimizer, epochs=1000):
+    for epoch in range(epochs):
+        for images, labels in loader: # X = images, y = labels
+            optimizer.zero_grad()
+            output = model(images)
+            loss = loss_fn(output, labels)
+            loss.backward()
+            optimizer.step()
+
+            if epoch % 200 == 0:
+                print(f'Epoch {epoch:4d}, loss {loss.item():.4f}')
+
+    return model
