dc2f57a214aa987a788a44de2bff753f15dfc0fe
chillz-the-first <vhenganitshilidzi846@gmail.com>
2026-07-01 18:35:09 +0200

    Created training loop function

---
+++ utilities/train.py
@@ -0,0 +1,9 @@
+def train(model, X, y, loss_fn, optimizer, epochs=1000):
+    for epoch in range(epochs):
+        optimizer.zero_grad()
+        output = model(X)
+        loss = loss_fn(output, y)
+        loss.backward()
+        optimizer.step()
+        
+    return model
